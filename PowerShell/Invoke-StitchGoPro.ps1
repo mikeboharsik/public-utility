a78f3d1f1138a7ed78548804b03f89c33b67ff37
Mike Boharsik <mboharsik@gmail.com>
2022-11-23 12:58:16 -0500

    Update Invoke-StitchGoPro.ps1 with latest changes

--- PowerShell/Invoke-StitchGoPro.ps1
+++ PowerShell/Invoke-StitchGoPro.ps1
@@ -1,28 +1,45 @@
+[CmdletBinding(SupportsShouldProcess = $true)]
 Param(
-	[Parameter(Mandatory=$true)]
-	[string] $FolderPath
+	[string] $SdCardPath = "P:",
+	[string] $ContentPath = "DCIM\100GOPRO",
+	[string] $DestinationFolder = "D:\wip"
 )
 
-$FolderPath = Resolve-Path $FolderPath
+$date = Get-Date -Format "yyyy-MM-dd"
 
-Push-Location $FolderPath
+$folderPath = "$PSScriptRoot\$date"
+New-Item -ItemType Directory -Path $folderPath -ErrorAction SilentlyContinue | Out-Null
+
+Push-Location $folderPath
+
+$fullContentPath = "$SdCardPath\$ContentPath"
+Write-Host $fullContentPath
 
 try {
-	$files = Get-ChildItem $FolderPath
+	$sdCardFiles = Get-ChildItem $fullContentPath
 		| Where-Object { $_.Extension -eq ".MP4" }
+
+	Write-Host $sdCardFiles
+
+	Copy-Item -Path $sdCardFiles -Destination $folderPath
+
+	$files = Get-ChildItem $folderPath
 		| Sort-Object { $_.LastWriteTime }
 
 	$start = $files[0]
 
 	$data = (exiftool -MediaCreateDate $start.FullName)
-	$date = Get-Date -Format "yyyy-MM-dd"
 	$mediaCreateDate = ($data.Split(': ')[1]) -Replace "(\d{4}):(\d{2}):(\d{2}) (\d{2}):(\d{2}):(\d{2})", "$($date)_`$4-`$5-`$6"
 
-	$joinedFilename = "$mediaCreateDate.mp4"
+	$joinedFilename = "$DestinationFolder\$mediaCreateDate.mp4"
 
 	$files | ForEach-Object { Add-Content "files.txt" "file $($_.Name)" }
 
 	ffmpeg -f concat -i "files.txt" -c copy $joinedFilename
+
+	Remove-Item $files
+	Remove-Item "$fullContentPath\*"
+	(New-Object -ComObject Shell.Application).NameSpace(17).ParseName($SdCardPath).InvokeVerb("Eject")
 } finally {
 	Remove-Item "files.txt" -ErrorAction SilentlyContinue
 
