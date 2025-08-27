dbe39410c865827d636af62de0d91f72b9662324
Mike Boharsik <mboharsik@gmail.com>
2025-08-27 16:40:32 -0400

    feat: delete Get-SensibleGoProFilename.ps1

--- PowerShell/Get-SensibleGoProFilename.ps1
+++
@@ -1,4 +0,0 @@
-[CmdletBinding()]
-Param([string] $Filename)
-
-return ($Filename[0..1] -Join '') + '_' + ($Filename[4..7] -Join '') + '_' + ($Filename[2..3] -Join '') + ($Filename[-4..-1] -Join '')
